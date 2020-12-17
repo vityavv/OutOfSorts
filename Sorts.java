b2f56d6fbafb1fc930e31eae71ef319439c93ef9
Victor Veytsman <vityavv@gmail.com>
2020-12-17 12:58:36 -0500

    Add insertion and selection sorts

--- Sorts.java
+++ Sorts.java
@@ -13,4 +13,27 @@ public class Sorts {
 			}
 		}
 	}
+	public static void selectionSort(int[] data) {
+		for (int i = 0; i < data.length - 1; i++) {
+			int smallestInd = i;
+			for (int j = 0; j < data.length; j++) {
+				if (data[j] < data[smallestInd]) smallestInd = j;
+			}
+			int temp = data[i];
+			data[i] = data[smallestInd];
+			data[smallestInd] = data[i];
+		}
+	}
+	public static void insertionSort(int[] data) {
+		for (int i = 1; i < data.length; i++) {
+			int temp = data[i];
+			int j = i - 1;
+			while(data[j] > temp) {
+				data[j+1] = data[j];
+				j--;
+				if (j == -1) break;
+			}
+			data[j+1] = temp;
+		}
+	}
 }
--- VTest.java
+++ VTest.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 public class VTest {
 	public static void main(String[] args) {
+		System.out.println("Bubble sort");
 		Random rng = new Random();
 		int[] randArr = randArray(rng);
 		System.out.println("Not sorted: ");
@@ -9,6 +10,39 @@ public class VTest {
 		Sorts.bubbleSort(randArr);
 		System.out.println("Sorted: ");
 		System.out.println(checkSorted(randArr));
+
+		System.out.println("Selection Sort");
+		randArr = randArray(rng);
+		System.out.println("Not sorted: ");
+		System.out.println(!checkSorted(randArr));
+		System.out.println("Sorting");
+		Sorts.selectionSort(randArr);
+		System.out.println("Sorted: ");
+		System.out.println(checkSorted(randArr));
+		int[] backwardsArr = new int[100];
+		for (int i = 0; i < backwardsArr.length; i++) backwardsArr[i] = 100 - i;
+		System.out.println("Not sorted: ");
+		System.out.println(!checkSorted(backwardsArr));
+		System.out.println("Sorting");
+		Sorts.selectionSort(backwardsArr);
+		System.out.println("Sorted: ");
+		System.out.println(checkSorted(backwardsArr));
+
+		System.out.println("Insertion Sort");
+		randArr = randArray(rng);
+		System.out.println("Not sorted: ");
+		System.out.println(!checkSorted(randArr));
+		System.out.println("Sorting");
+		Sorts.insertionSort(randArr);
+		System.out.println("Sorted: ");
+		System.out.println(checkSorted(randArr));
+		for (int i = 0; i < backwardsArr.length; i++) backwardsArr[i] = 100 - i;
+		System.out.println("Not sorted: ");
+		System.out.println(!checkSorted(backwardsArr));
+		System.out.println("Sorting");
+		Sorts.insertionSort(backwardsArr);
+		System.out.println("Sorted: ");
+		System.out.println(checkSorted(backwardsArr));
 	}
 	public static boolean checkSorted(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
